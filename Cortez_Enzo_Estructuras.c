92b1a8345a4b9b9d2379e20f246cf3d5d3561d6a
Enzo Cortez <enzo.cortez@udla.edu.ec>
2024-01-16 15:16:02 -0500

    Definicion de la estructura principal y la funcion main junto al diseño del menu

--- Cortez_Enzo_Estructuras.c
+++ Cortez_Enzo_Estructuras.c
@@ -13,3 +13,47 @@ struct alumno
 // Prototipos de las funciones
 void ingresarAlumnos(struct alumno lista[], int n);
 void mostrarAlumnos(struct alumno lista[], int n);
+
+int main()
+{
+    int opcion;
+    int n;
+    
+    // Solicitar al usuario la cantidad de alumnos
+    printf("Ingrese la cantidad de alumnos: ");
+    scanf("%d", &n);
+
+    // Crear un arreglo de estructuras alumno con capacidad para n alumnos
+    struct alumno listaAlumnos[n];
+
+    do
+    {
+        // Menú principal
+        printf("\nMenú:\n");
+        printf("1. Ingresar alumnos\n");
+        printf("2. Mostrar alumnos\n");
+        printf("3. Salir\n");
+        printf("Seleccione una opción: ");
+        scanf("%d", &opcion);
+
+        switch (opcion)
+        {
+        case 1:
+            ingresarAlumnos(listaAlumnos, n);
+            break;
+
+        case 2:
+            mostrarAlumnos(listaAlumnos, n);
+            break;
+
+        case 3:
+            printf("Saliendo del programa.\n");
+            break;
+
+        default:
+            printf("Opción no válida. Intente de nuevo.\n");
+        }
+    } while (opcion != 3);
+
+    return 0;
+}
