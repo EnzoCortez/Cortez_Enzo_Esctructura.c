a5a867f265ea62eae330d14a0bb951b0ee3550f6
Enzo Cortez <enzo.cortez@udla.edu.ec>
2024-01-16 15:13:33 -0500

    Definicion de la estructura alumno

---
+++ Cortez_Enzo_Estructuras.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+// Definición de la estructura alumno
+struct alumno
+{
+    int matricula;
+    char nombre[50];
+    char direccion[50];
+    char materia[50];
+    float nota;
+};
